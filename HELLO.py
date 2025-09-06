56cf3ceab87e6160f0490e2315a8809aa7e26f66
maiatsirekidze <maia.tsirekidzemts@gmail.com>
2025-09-06 15:30:36 +0400

    Add files via upload

---
+++ HELLO.py
@@ -0,0 +1,30 @@
+# print("hello")
+# for  i in range(5):
+#     if i%2==0:
+#         print("luwi")
+#     else:
+#         print("kenti")
+numbers = [1,2,3,4,5,"erti"]
+numbers.append("tetmeti")
+numbers.insert(1," meore damatebuli")
+numbers.remove(1)
+# for i in range(7):
+#     print(numbers[i], end=" ")
+# del numbers[0:5]
+# daTvla
+a=numbers.count(2)
+
+print(a)
+numbers.reverse()
+numbers.sort()
+print(numbers)
+print(numbers)    
+a=len(numbers)
+for i in range(a):
+    print(numbers[i])
+
+
+   
+
+
+        
